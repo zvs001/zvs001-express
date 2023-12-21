0b79eb38fca9a60c404bf8ca6e58abed8d99d30c
zvsx001 <zvsx001@gmail.com>
2023-12-21 19:15:54 +0200

    apply body-parser by default

--- src/createServer.ts
+++ src/createServer.ts
@@ -2,8 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import addRequestId from 'express-request-id'
 import robots from 'express-robots-txt'
+import bodyParser from "body-parser";
 
-function createServer() {
+interface CreateServerParams {
+  parsers?: {
+    json?: boolean
+    urlEncoded?: boolean
+    text?: boolean
+    limit?: string
+  }
+}
+function createServer(params?: CreateServerParams) {
+  const { parsers } = params || {}
+  const { json = true, urlEncoded, text, limit } = parsers || {}
   const app = express()
 
   app.set('trust proxy', 1) // it changes proxy x-forwarded-for ip as client ip
@@ -15,6 +26,20 @@ function createServer() {
     UserAgent: '*',
     Disallow: '/'
   }))
+
+  if(json) {
+    app.use(bodyParser.json({ limit }))
+  }
+  if(urlEncoded) {
+    app.use(bodyParser.urlencoded({
+      extended: true,
+      limit,
+    }))
+  }
+
+  if(text) {
+    app.use(bodyParser.text({ limit }))
+  }
 
   return app
 }
